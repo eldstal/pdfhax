276ca70f697f07b620e6408b61a512f4d6f9e471
Albin Eldstål-Damlin <laeder.keps@gmail.com>
2018-10-30 19:09:28 +0100

    Some more work on mapping out 3D Javascript

---
+++ js/cubus_and_ballus.js
@@ -0,0 +1,5 @@
+console.println("3D Script loaded");
+background.setColor(new Color(0.98, 0.82, 0.98), new Color(0.88, 0.72, 0.88));
+app.alert("3D script loaded.",3);
+
+
---
+++ js/cubus_and_ballus_document.js
@@ -0,0 +1,7 @@
+console.println("Document Script loaded.");
+
+pageIndex = this.pageNum;
+
+ctx = this.getAnnots3D(pageIndex)[0].context3D;
+
+ctx.background.setColor(new Color(0.98, 0.82, 0.98), new Color(0.88, 0.72, 0.88));
