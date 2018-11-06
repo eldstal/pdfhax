960b3dd1e963b32d00ecc5bda82ef647d6e4eccd
Albin <eldstal@chalmers.se>
2018-11-06 20:44:01 +0100

    A few parts of a rudimentary game engine

--- js/game_vec.js
+++ js/game_vec.js
@@ -1,45 +1,35 @@
-
-
-// Crappy breakout clone using PDF Annotations
-
-
 PAD_Y = 50;
 PAD_W = 180;
 PAD_H = 30;
 
+BRICK_W = 70;
+BRICK_H = 20;
 
 
 while (!PDFUN_LOADED) {}
 
+pdfun.clear();
 
-paddle = {};
-paddle.sprite = this.addAnnot(
-       { type: "Square",
-         page: 0,
-         rect: [0, PAD_Y, PAD_W, PAD_Y+PAD_H],
-         fillColor: ["RGB", 0.2, 0.2, 0.25 ],
-         strokeColor: ["RGB", 0.1, 0.1, 0.15]
+paddle = pdfun.entity(
+         { type: "Square",
+           page: 0,
+           rect: [0, PAD_Y, PAD_W, PAD_Y+PAD_H],
+           fillColor: ["RGB", 0.2, 0.2, 0.25 ],
+           strokeColor: ["RGB", 0.1, 0.1, 0.15]
+         });
+paddle.width = PAD_W;
+paddle.height = PAD_H;
+paddle.pos = [ 0, PAD_Y ];
+paddle.velocity = [ 20, 0 ];
 
-       });
-paddle.pos = 300;
+
+/* TODO: Add bricks */
 
 
-
-
-dir = 1;
-speed = 20;
-function tick() {
-  paddle.pos += dir * speed;
-  if (paddle.pos < 0) speed *= -1;
-  if (paddle.pos > (1280-PAD_W)) speed *= -1;
-
-  paddle.sprite.rect = [ paddle.pos, PAD_Y, paddle.pos + PAD_W, PAD_Y + PAD_H ];
-
+pdfun.tick = function () {
+  if (paddle.pos[0] < 0) paddle.velocity[0] *= -1;
+  if (paddle.pos[0] > (1280-PAD_W)) paddle.velocity[0] *= -1;
 }
-
-main_ticker = app.setInterval("tick()", 40);
-
-
 
 
 
--- js/pdfun_vec/engine.js
+++ js/pdfun_vec/engine.js
@@ -1,7 +1,74 @@
+var PDFUN_LOADED = false;
 
-var PDFUN_LOADED = true;
+var pdfun = {}
+pdfun.document = this
 
-function pdfun_alert() {
+pdfun.entities = []
+
+pdfun.alert = function() {
   app.alert("PDFUN engine loaded.", 3);
 }
 
+/* Clear any old annotations, etc */
+pdfun.clear = function() {
+  annots = this.document.getAnnots();
+  if (annots) {
+    for (a=0; a<annots.length; a++) {
+      annots[a].destroy();
+    }
+  }
+}
+
+
+pdfun.entity = function(initializer) {
+  ent = {};
+
+  ent.sprite = this.document.addAnnot(initializer);
+  ent.width = 1;
+  ent.height = 1;
+  ent.velocity = [ 0, 0 ];
+  ent.pos = [ 0, 0 ];
+
+  this.entities.push(ent)
+
+  return ent;
+}
+
+pdfun.move = function(ent) {
+
+  ent.pos[0] += ent.velocity[0];
+  ent.pos[1] += ent.velocity[1];
+
+  ent.sprite.rect = [ ent.pos[0],
+                      ent.pos[1],
+                      ent.pos[0] + ent.width,
+                      ent.pos[1] + ent.height ];
+}
+
+pdfun.destroy = function(ent) {
+  var index = this.entities.indexOf(ent);
+  if (index > -1) {
+    this.entities.splice(index, 1);
+  }
+
+  // Remove from the document as well
+  ent.sprite.destroy();
+}
+
+pdfun.tick = null;
+pdfun.main = function() {
+
+  // Call the user's frame function
+  if (this.tick) {
+    this.tick();
+  }
+
+  // Move all objects
+  for (e=0; e<this.entities.length; e++) {
+    this.move(this.entities[e]);
+  }
+}
+
+pdfun.ticker = app.setInterval("pdfun.main()", 20);
+
+PDFUN_LOADED = true;
